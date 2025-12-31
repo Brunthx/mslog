76347266b532bc4dd5b3908ab803974488a53cdd
Brunthx <a15992465412@163.com>
2025-12-31 22:40:20 +0800

    [mslog] modify mem pool module by Edison

--- mslog_mem_pool.c
+++ mslog_mem_pool.c
@@ -1,105 +1,66 @@
 #define _POSIX_C_SOURCE 200112L
 #include "mslog_mem_pool.h"
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
 
-size_t i,j;
+mslog_mem_pool_t g_mslog_mem_pool = {
+    .pool_buf = NULL,
+    .pool_size = 0,
+    .used_size = 0
+};
 
-int mslog_mem_pool_init(mslog_mem_pool_t *pool, size_t node_buf_size, size_t node_count){
-    if ( pool == NULL || node_buf_size == 0 || node_count == 0 ){
+int mslog_mem_pool_init(void){
+    g_mslog_mem_pool.pool_size = MSLOG_MEM_POOL_BLOCK_SIZE * MSLOG_MEM_POOL_MAX_BLOCKS;
+    g_mslog_mem_pool.pool_buf = (char *)malloc(g_mslog_mem_pool.pool_buf);
+    if ( g_mslog_mem_pool.pool_buf == NULL )
+    {
         return -1;
     }
-
-    memset(pool, 0, sizeof(mslog_mem_pool_t));
-
-    pool->nodes = (mslog_buf_node_t *)calloc(node_count, sizeof(mslog_buf_node_t));
-    if ( pool->nodes == NULL ){
-        return -2;
-    }
-
-    pool->node_count = node_count;
-    pool->node_buf_size = node_buf_size;
-    for ( i = 0; i < node_count; i++ ){
-        pool->nodes[i].buf = (char *)malloc(node_buf_size);
-        if ( pool->nodes[i].buf == NULL ){
-            for ( j = 0; j < i; j++ ){
-                free(pool->nodes[j].buf);
-            }
-            free(pool->nodes);
-            pool->nodes = NULL;
-            return -2;
-        }
-        pool->nodes[i].size = node_buf_size;
-        pool->nodes[i].occupied = 0;//free, unused
-    }
-
-    int ret = pthread_mutex_init(&pool->pool_mutex, NULL);
-    if ( ret != 0 ){
-        for ( i = 0; i < node_count; i++ ){
-            free(pool->nodes[i].buf);
-        }
-        free(pool->nodes);
-        pool->nodes = NULL;
-        return -3;
-    }
+    memset(g_mslog_mem_pool.pool_buf, 0, g_mslog_mem_pool.pool_size);
+    pthread_mutex_init(&g_mslog_mem_pool.pool_mutex);
     return 0;
 }
 
-mslog_buf_node_t *mslog_mem_pool_alloc(mslog_mem_pool_t *pool){
-    if ( pool == NULL || pool->nodes == NULL || pool->node_count == 0 ){
-        return NULL;
+void *mslog_mem_pool_alloc(size_t size){
+    if ( size == 0 || size > g_mslog_mem_pool.pool_size )
+    {
+        return malloc(size);
     }
 
-    pthread_mutex_lock(&pool->pool_mutex);
-
-    mslog_buf_node_t *free_node = NULL;
-    for ( i = 0; i < pool->node_count; i++ ){
-        if ( pool->nodes[i].occupied == 0){
-            pool->nodes[i].occupied = 1;
-            free_node = &pool->nodes[i];
-            break;
-        }
+    pthread_mutex_lock(&g_mslog_mem_pool.pool_mutex);
+    void *ptr = NULL;
+    if ( g_mslog_mem_pool.used_size + size <= g_mslog_mem_pool.pool_size)
+    {
+        ptr = g_mslog_mem_pool.pool_buf + g_mslog_mem_pool.used_size;
     }
-
-    pthread_mutex_unlock(&pool->pool_mutex);
-    
-    return free_node;
+    else
+    {
+        ptr = malloc(size);
+    }
+    pthread_mutex_unlock(&g_mslog_mem_pool.pool_mutex);
+    return ptr;
 }
 
-void mslog_mem_pool_free(mslog_mem_pool_t *pool, mslog_buf_node_t *node){
-    if ( pool == NULL || pool->nodes == NULL || node == NULL ){
+void mslog_mem_free(void *ptr){
+    if ( ptr == NULL )
+    {
         return;
     }
-
-    pthread_mutex_lock(&pool->pool_mutex);
-
-    node->occupied = 0;
-
-    pthread_mutex_unlock(&pool->pool_mutex);
+    if ( ptr >= (void *)g_mslog_mem_pool.pool_buf && ptr < (void *)(g_mslog_mem_pool.pool_buf + g_mslog_mem_pool.pool_size) )
+    {
+        pthread_mutex_lock(&g_mslog_mem_pool.pool_mutex);
+        memset(ptr, 0, 1);
+        pthread_mutex_unlock(&g_mslog_mem_pool.pool_mutex);
+        return;
+    }
+    free(ptr);
 }
 
-void mslog_mem_pool_destroy(mslog_mem_pool_t *pool){
-    if ( pool == NULL || pool->nodes == NULL ){
-        return;
+void mslog_mem_pool_deinit(void){
+    pthread_mutex_lock(&g_mslog_mem_pool.pool_mutex);
+    if ( g_mslog_mem_pool.pool_buf != NULL )
+    {
+        free(g_mslog_mem_pool.pool_buf);
+        g_mslog_mem_pool.pool_buf = NULL;
     }
-
-    pthread_mutex_lock(&pool->pool_mutex);
-
-    for ( i = 0; i < pool->node_count; i++ ){
-        if ( pool->nodes[i].buf != NULL ){
-            free(pool->nodes[i].buf);
-            pool->nodes[i].buf = NULL;
-        }
-    }
-
-    free(pool->nodes);
-    pool->nodes = NULL;
-    pool->node_count = 0;
-    pool->node_buf_size = 0;
-
-    pthread_mutex_unlock(&pool->pool_mutex);
-    pthread_mutex_destroy(&pool->pool_mutex);
-
-    memset(pool, 0, sizeof(mslog_mem_pool_t));
+    pthread_mutex_unlock(&g_mslog_mem_pool.pool_mutex);
+    pthread_mutex_destroy(&g_mslog_mem_pool.pool_mutex);
 }
--- mslog_mem_pool.h
+++ mslog_mem_pool.h
@@ -2,28 +2,26 @@
 #ifndef __MSLOG_MEM_POOL_H__
 #define __MSLOG_MEM_POOL_H__
 
+#include <stdio.h>
 #include <pthread.h>
-#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct mslog_buf_node{
-    char *buf;
-    size_t size;
-    int occupied;
-}mslog_buf_node_t;
+#define MSLOG_MEM_POOL_BLOCK_SIZE   ( 4096 )
+#define MSLOG_MEM_POOL_MAX_BLOCKS   ( 1024 )
 
 typedef struct{
-    mslog_buf_node_t *nodes;
-    size_t node_count;
-    size_t node_buf_size;
+    char *pool_buf;
+    size_t pool_size;
+    size_t used_size;
     pthread_mutex_t pool_mutex;
 }mslog_mem_pool_t;
 
-int mslog_mem_pool_init(mslog_mem_pool_t *pool, size_t node_buf_size, size_t node_count);
+extern mslog_mem_pool_t g_mslog_mem_pool;
 
-mslog_buf_node_t *mslog_mem_pool_alloc(mslog_mem_pool_t *pool);
-
-void mslog_mem_pool_free(mslog_mem_pool_t *pool, mslog_buf_node_t *node);
-
-void mslog_mem_pool_destroy(mslog_mem_pool_t *pool);
+int mslog_mem_pool_init(void);
+void *mslog_mem_alloc(size_t size);
+void mslog_mem_free(void *ptr);
+void mslog_mem_pool_deinit(void);
 
 #endif//__MSLOG_MEM_POOL_H__
