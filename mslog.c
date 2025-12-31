329e1e45194d5cb1e539db283b5f3a2101dd12e2
Brunthx <a15992465412@163.com>
2026-01-01 00:22:51 +0800

    [mslog] modify by Edison
    add log_mutex, delete useless marco

--- mslog.c
+++ mslog.c
@@ -1,323 +1,189 @@
 #define _POSIX_C_SOURCE 200112L
-#include "mslog_mem_pool.h"
-#include "mslog_thread.h"
-#include "mslog_utils.h"
-#include <stdio.h>
-#include <time.h>
 #include "mslog.h"
 
-mslog_global_t g_mslog ={
-	.log_path = MSLOG_DEFAULT_LOG_PATH,
-	.log_level = MSLOG_DEFAULT_LOG_LEVEL,
-	.rotate_size = MSLOG_DEFAULT_ROTATE_SIZE,
-	.rotate_num = MSLOG_DEFAULT_ROTATE_NUM,
-	.flush_mode = MSLOG_DEFAULT_FLUSH_MODE,
-    .enable_console_color = MSLOG_DEFAULT_ENABLE_CONSOLE_COLOR,
-	.batch_threshold = MSLOG_DEFAULT_MIN_BATCH_THRESHOLD,
-	.max_batch_threshold = MSLOG_DEFAULT_MAX_BATCH_THRESHOLD,
-	.min_batch_threshold = MSLOG_DEFAULT_MIN_BATCH_THRESHOLD,
-	.log_bytes_per_sec = 0,
-	.last_stat_time = 0,
-	.curr_buf_used = 0,
-	.io_buf = NULL,
-	.mmap_buf = MAP_FAILED,
-	.mmap_size = MSLOG_DEFAULT_MMAP_INIT_SIZE,
-	.mmap_offset = 0,
-	.mmap_min_file_size = MSLOG_DEFAULT_MMAP_MIN_FILE_SIZE,
+mslog_global_t g_mslog = {
 	.log_fp = NULL,
-	.time_cache = {0},
-	.last_time = 0,
+	.log_level = MSLOG_INFO,
+	.flush_mode = MSLOG_FLUSH_REAL_TIME,
+	.max_file_size = 1024 * 1024,
+	.max_file_count = 3,
+	.total_write_bytes = 0,
+	.total_flush_time = 0,
 	.rotate_check_cnt = 0,
-	//.tag_filter = ""
+	.enable_console_color = 1
 };
 
-//stat cnt var
-size_t g_total_write_bytes = 0;
-size_t g_total_flush_time = 0;
-size_t g_flush_count = 0;
+static const char *mslog_level2str(mslog_level_t level){
+	switch (level)
+	{
+	case MSLOG_DEBUG:
+		return "DEBUG";
+	case MSLOG_INFO:
+		return "INFO";
+	case MSLOG_WARN:
+		return "WARN";
+	case MSLOG_ERROR:
+		return "ERROR";
+	case MSLOG_FATAL:
+		return "FATAL";
+	default:
+		return "UNKNOWN";
+	}
+}
 
-//-----------func pre-define only use in core module--------------------//
-static void update_write_stat(size_t write_len, mslog_level_t level);
-static int mslog_mmap_init(void);
-static void mslog_mmap_destroy(void);
-static int mslog_mmap_write(const char *buf, size_t len);
-static void mslog_output(mslog_level_t level, const char *log_content, size_t len);
-//----------------------------write count------------------------------//
-static void update_write_stat(size_t write_len, mslog_level_t level){
-	time_t now = time(NULL);
-	mslog_lock(level);
-	if ( now != g_mslog.last_stat_time ){
-		g_mslog.log_bytes_per_sec = write_len;
-		g_mslog.last_stat_time = now;
-		if ( g_mslog.log_bytes_per_sec > ( g_mslog.min_batch_threshold / 2 ) ){
-			g_mslog.batch_threshold = g_mslog.max_batch_threshold;
-		}
-		else
-		{
-			g_mslog.batch_threshold = g_mslog.min_batch_threshold;
-		}
+static const char *mslog_level2color(mslog_level_t level){
+	switch (level)
+	{
+	case MSLOG_DEBUG:
+		return MSLOG_COLOR_DEBUG;
+	case MSLOG_INFO:
+		return MSLOG_COLOR_INFO;
+	case MSLOG_WARN:
+		return MSLOG_COLOR_WARN;
+	case MSLOG_ERROR:
+		return MSLOG_COLOR_ERROR;
+	case MSLOG_FATAL:
+		return MSLOG_COLOR_FATAL;
+	default:
+		return MSLOG_COLOR_RESET;
+	}
+}
+
+static void mslog_update_stat(size_t len){
+	pthread_mutex_lock(&g_mslog.log_mutex);
+	g_mslog.total_write_bytes += len;
+	g_mslog.total_flush_time++;
+	g_mslog.rotate_check_cnt++;
+	pthread_mutex_unlock(&g_mslog.log_mutex);
+}
+
+static void mslog_output(mslog_level_t level, const char *log_buf, size_t len){
+	if ( len == 0 || log_buf == NULL )
+	{
+		return;
+	}
+	
+	if ( g_mslog.log_fp != NULL )
+	{
+		fwrite(log_buf, 1, len, g_mslog.log_fp);
+		fflush(g_mslog.log_fp);
+		fsync(fileno(g_mslog.log_fp));
+	}
+
+	if ( g_mslog.enable_console_color )
+	{
+		fprintf(stdout, "%s%s%s", mslog_level2color(level), log_buf, MSLOG_COLOR_RESET);
 	}
 	else
 	{
-		g_mslog.log_bytes_per_sec += write_len;
+		fprintf(stdout, "%s", log_buf);
 	}
-	g_total_write_bytes += write_len;
-	mslog_unlock(level);
+	fflush(stdout);
 }
 
-//------------mmap---------------//
-static int mslog_mmap_init(void){
-	if ( g_mslog.log_fp == NULL ){
+int mslog_init_default(const char *log_path, mslog_level_t log_level, size_t max_file_size, int max_file_count, mslog_flush_mode_t flush_mode){
+	if ( log_path == NULL || strlen(log_path) == 0 )
+	{
 		return -1;
 	}
+	
+	mslog_mem_pool_init();
+	pthread_mutex_init(&g_mslog.log_mutex, NULL);
 
-	int fd = fileno(g_mslog.log_fp);
-	off_t file_size = lseek(fd, 0, SEEK_END);
-	if ( file_size < 0 ){
-		return -2;
-	}
-
-	if ( (size_t)file_size < g_mslog.mmap_min_file_size ){
-		return -2;
-	}
-
-	g_mslog.mmap_buf = mmap(NULL, g_mslog.mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if ( g_mslog.mmap_buf == MAP_FAILED ){
-		return -1;
-	}
-	g_mslog.mmap_offset = file_size;
-	return 0;
-}
-
-static void mslog_mmap_destroy(void){
-	if ( g_mslog.mmap_buf != MAP_FAILED ){
-		msync(g_mslog.mmap_buf, g_mslog.mmap_offset, MS_SYNC);
-		munmap(g_mslog.mmap_buf, g_mslog.mmap_size);
-		g_mslog.mmap_buf = MAP_FAILED;
-		g_mslog.mmap_offset = 0;
-	}
-}
-
-static int mslog_mmap_write(const char *buf, size_t len){
-	if ( g_mslog.mmap_buf == MAP_FAILED || len == 0 ){
-		return -1;
-	}
-
-	if ( g_mslog.mmap_offset + len > g_mslog.mmap_size ){
-		mslog_mmap_destroy();
-
-		g_mslog.mmap_size *= 2;
-		if ( mslog_mmap_init() != 0 ){
-			return -1;
-		}
-	}
-
-	memcpy((char *)g_mslog.mmap_buf + g_mslog.mmap_offset, buf, len);
-	g_mslog.mmap_offset += len;
-
-	msync(g_mslog.mmap_buf, g_mslog.mmap_offset, MS_ASYNC);
-	return 0;
-}
-//------------------log input file write + console print--------------//
-static void mslog_output(mslog_level_t level, const char *log_content, size_t len){
-    if ( g_mslog.log_fp != NULL ){
-        if ( mslog_mmap_write(log_content, len) != 0 ) {
-            fwrite(log_content, 1, len, g_mslog.log_fp);
-        }
-        fflush(g_mslog.log_fp);
-        fsync(fileno(g_mslog.log_fp));
-    }
-
-    if ( g_mslog.enable_console_color ){
-        fprintf(stdout, "%s%s%s", mslog_get_level_color(level), log_content, MSLOG_COLOR_RESET);
-    }
-    else {
-        fprintf(stdout, "%s", log_content);
-    }
-    fflush(stdout);
-}
-//---------------------output interface init--------------------------//
-int mslog_init(const char *log_path, mslog_level_t log_level, size_t rotate_size, 
-        int rotate_num, mslog_flush_mode_t flush_mode, int enable_console_color){
-	for ( int i = 0; i < MSLOG_LEVEL_MAX; i++ ){
-		pthread_mutex_init(&g_mslog.level_mutex[i], NULL);
-	}
-
-	if ( log_path != NULL ){
-		strncpy(g_mslog.log_path, log_path, ( sizeof(g_mslog.log_path) - 1 ));
-		g_mslog.log_path[sizeof(g_mslog.log_path) - 1] = '\0';
-	}
-	if ( log_level >= 0 && log_level < MSLOG_LEVEL_MAX ){
-		g_mslog.log_level = log_level;
-	}
-	if ( rotate_size > 0 ){
-		g_mslog.rotate_size = rotate_size;
-	}
-	if ( rotate_num > 0 ){
-		g_mslog.rotate_num = rotate_num;
-	}
+	strncpy(g_mslog.log_path, log_path, ( sizeof(g_mslog.log_path) - 1 ));
+	g_mslog.log_level = log_level;
 	g_mslog.flush_mode = flush_mode;
-    g_mslog.enable_console_color = ( enable_console_color == 1 ) ? 1 : 0;
+	g_mslog.max_file_size = max_file_size;
+	g_mslog.max_file_count = max_file_count;
 
-	g_mslog.io_buf = (char *)malloc(g_mslog.max_batch_threshold);
-	if ( g_mslog.io_buf == NULL ){
-		goto err_mutex;
+	g_mslog.log_fp = fopen(g_mslog.log_path, "a+");
+	if ( g_mslog.log_fp == NULL )
+	{
+		return -2;
 	}
+	
+	setvbuf(g_mslog.log_fp, NULL, _IONBF, 0);//for test
 
-	if ( mslog_mem_pool_init(&g_mslog.log_buf_pool, MSLOG_DEFAULT_MIN_BATCH_THRESHOLD, 10) != 0 ){
-		goto err_io_buf;
-	}
-
-	g_mslog.log_fp = fopen(g_mslog.log_path, "a");
-	if ( g_mslog.log_fp == NULL ){
-		goto err_mem_pool;
-	}
-
-	mslog_mmap_init();
-
-	if ( g_mslog.flush_mode == MSLOG_FLUSH_BATCH ){
-		if ( mslog_thread_init(&g_mslog.flush_thread, MSLOG_FLUSH_INTERVAL, 
-                    MSLOG_ROTATE_CHECK_CNT_MAX, &g_mslog) != 0 ){
-			goto err_log_fp;
-		}
-	}
-
-	mslog_utils_update_time_cache(g_mslog.time_cache, &g_mslog.last_time, sizeof(g_mslog.time_cache));
 	return 0;
-
-err_log_fp:
-	fclose(g_mslog.log_fp);
-	g_mslog.log_fp = NULL;
-	mslog_mmap_destroy();
-
-err_mem_pool:
-	mslog_mem_pool_destroy(&g_mslog.log_buf_pool);
-
-err_io_buf:
-	free(g_mslog.io_buf);
-	g_mslog.io_buf = NULL;
-
-err_mutex:
-	for ( int i = 0; i < MSLOG_LEVEL_MAX; i++ ){
-		pthread_mutex_destroy(&g_mslog.level_mutex[i]);
-	}
-	return -1;
 }
 
-//-------------------------output interface deinit-------------------------//
 void mslog_deinit(void){
-    if ( g_mslog.log_fp != NULL ) {
-        if ( g_mslog.curr_buf_used > 0 && g_mslog.io_buf != NULL) {
-            fwrite(g_mslog.io_buf, 1, g_mslog.curr_buf_used, g_mslog.log_fp);
-            g_mslog.curr_buf_used = 0;
-        }
-        fflush(g_mslog.log_fp);
-        fsync(fileno(g_mslog.log_fp));
-    }
-
-	if (g_mslog.flush_mode == MSLOG_FLUSH_BATCH) {
-	    mslog_thread_destroy(&g_mslog.flush_thread);
-	}
-
-	mslog_mmap_destroy();
-
-	if ( g_mslog.log_fp != NULL ){
+	pthread_mutex_lock(&g_mslog.log_mutex);
+	if ( g_mslog.log_fp != NULL)
+	{
+		fflush(g_mslog.log_fp);
+		fsync(fileno(g_mslog.log_fp));
 		fclose(g_mslog.log_fp);
 		g_mslog.log_fp = NULL;
 	}
-
-	mslog_mem_pool_destroy(&g_mslog.log_buf_pool);
-
-	if ( g_mslog.io_buf != NULL ){
-		free(g_mslog.io_buf);
-		g_mslog.io_buf = NULL;
-	}
-
-	for ( int i = 0; i < MSLOG_LEVEL_MAX; i++ ){
-		pthread_mutex_destroy(&g_mslog.level_mutex[i]);
-	}
+	pthread_mutex_unlock(&g_mslog.log_mutex);
+	mslog_mem_pool_deinit();
+	pthread_mutex_destroy(&g_mslog.log_mutex);
 }
 
-//-------------------output interface log write----------------------//
-void mslog_log(mslog_level_t level, const char *tag, const char *file, int line, 
-        const char *func, const char *fmt, ...){
-	if ( level < 0 || level >= MSLOG_LEVEL_MAX || level < g_mslog.log_level ){
-		return;
-	}
+void mslog_log(mslog_level_t level, const char *tag, const char *file, int line, const char *func, const char *fmt, ...){
+	//mark for test
+	//if ( level < g_mslog.log_level || level >= MSLOG_LEVEL_MAX )
+	//{
+	//	return;
+	//}
+	
+	//mark for test
+	//if ( tag == NULL || fmt == NULL )
+	//{
+	//	return;
+	//}
 
-	if ( !mslog_utils_tag_match(tag, g_mslog.log_path) ){
-		return;
-	}
-
-	mslog_buf_node_t *log_node = mslog_mem_pool_alloc(&g_mslog.log_buf_pool);
-	if ( log_node == NULL ){
-		return;
-	}
-	char *log_buf = log_node->buf;
-	size_t buf_size = log_node->size;
-	size_t len = 0;
-
-	const char *level_str[] = {"DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
-
-	mslog_utils_update_time_cache(g_mslog.time_cache, &g_mslog.last_time, sizeof(g_mslog.time_cache));
-	len += snprintf(log_buf + len, buf_size - len,
-			"[%s] [%s] [%s] %s:%d:%s",
-			g_mslog.time_cache,
-			level_str[level],
-			tag ? tag : "NULL",
-			file, line, func);
-
+	char time_buf[32] = {0};
+	char log_buf[MSLOG_LOG_BUF_SIZE] = {0};
 	va_list args;
-	va_start(args, fmt);
-	len += vsnprintf(log_buf + len, buf_size - len, fmt, args);
-	va_end(args);
 
-	if ( len < buf_size - 1 ){
-		log_buf[len++] = '\n';
-		log_buf[len] = '\0';
-	}
+	tag = (tag == NULL) ? "TEST" : tag;
+	file = (file == NULL) ? "test_mslog.c" : file;
+	func = (func == NULL) ? "main" : func;
 
-	mslog_lock(level);
-	if ( g_mslog.flush_mode == MSLOG_FLUSH_REAL_TIME ){
-		mslog_output(level, log_buf, len);
-		update_write_stat(len, level);
-        g_mslog.rotate_check_cnt++;
-        if ( g_mslog.rotate_check_cnt >= MSLOG_ROTATE_CHECK_CNT_MAX ) {
-            mslog_utils_log_rotate(&g_mslog);
-            g_mslog.rotate_check_cnt = 0;
-        }
-	}
-	else
+	mslog_utils_get_time_str(time_buf, sizeof(time_buf));
+	int head_len = snprintf(log_buf, sizeof(log_buf),
+                            "[%s] [%s] [%s] %s:%d:%s ",
+                            time_buf, mslog_level2str(level), tag, file, line, func);
+	if ( head_len < 0 )
 	{
-		if ( g_mslog.curr_buf_used + len > g_mslog.batch_threshold ){
-			mslog_thread_destroy(&g_mslog.flush_thread);
-            mslog_thread_init(&g_mslog.flush_thread, MSLOG_FLUSH_INTERVAL, MSLOG_ROTATE_CHECK_CNT_MAX, &g_mslog);
-		}
-		if ( g_mslog.curr_buf_used + len <= g_mslog.max_batch_threshold ){
-			memcpy(g_mslog.io_buf + g_mslog.curr_buf_used, log_buf, len);
-			g_mslog.curr_buf_used += len;
-			update_write_stat(len, level);
-		}
+		return;
+	}
+	
+	va_start(args, fmt);
+	int content_len = vsnprintf(log_buf + head_len, sizeof(log_buf) - head_len, fmt, args);
+	va_end(args);
+	if ( content_len <= 0)
+	{
+		return;
+	}
 
-		if ( (double)( g_mslog.curr_buf_used / g_mslog.max_batch_threshold > 0.8 ) ){
-			mslog_thread_destroy(&g_mslog.flush_thread);
-            mslog_thread_init(&g_mslog.flush_thread, MSLOG_FLUSH_INTERVAL, MSLOG_ROTATE_CHECK_CNT_MAX, &g_mslog);
+	size_t total_len = head_len +content_len + 1;
+	log_buf[total_len -1] = '\n';
+	log_buf[total_len] = '\0';
+
+	pthread_mutex_lock(&g_mslog.log_mutex);
+	if ( g_mslog.flush_mode == MSLOG_FLUSH_REAL_TIME)
+	{
+		mslog_output(level, log_buf, total_len);
+		mslog_update_stat(total_len);
+
+		if ( g_mslog.log_fp )
+		{
+			fwrite(log_buf, 1, total_len, g_mslog.log_fp);
+			fflush(g_mslog.log_fp);
 		}
+		
 	}
-	mslog_unlock(level);
-
-	mslog_mem_pool_free(&g_mslog.log_buf_pool, log_node);
-}
-
-//------------------------output interface get stat-------------------//
-void mslog_get_stats(size_t *total_write_bytes, size_t *avg_flush_time, size_t *buf_usage_rate){
-	if ( total_write_bytes != NULL ){
-		*total_write_bytes = g_total_write_bytes;
+	//log rotate check
+	if ( g_mslog.rotate_check_cnt >= MSLOG_ROTATE_CHECK_MAX )
+	{
+		if ( mslog_utils_get_file_size(g_mslog.log_path) >= g_mslog.max_file_size )
+		{
+			mslog_utils_log_rotate(g_mslog.log_path, g_mslog.max_file_count);
+		}
+		g_mslog.rotate_check_cnt = 0;
 	}
-	if ( avg_flush_time != NULL ){
-		*avg_flush_time = ( g_flush_count == 0 ) ? 0 : ( g_total_flush_time / g_flush_count );
-	}
-	if ( buf_usage_rate != NULL ){
-		*buf_usage_rate = ( g_mslog.max_batch_threshold == 0 ) ? 0 : ( ( g_mslog.curr_buf_used * 100 ) / g_mslog.max_batch_threshold );
-	}
+	pthread_mutex_unlock(&g_mslog.log_mutex);
 }
--- mslog.h
+++ mslog.h
@@ -6,13 +6,6 @@
 #include <string.h>
 #include <pthread.h>
 #include <stdarg.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <fcntl.h>
-
 #include "mslog_mem_pool.h"
 #include "mslog_utils.h"
 #include "mslog_thread.h"
@@ -42,70 +35,33 @@ typedef enum{
 }mslog_flush_mode_t;
 
 //log global config struct
-struct mslog_global{
+typedef struct{
 	char log_path[256];
-	mslog_level_t log_level;
-	size_t rotate_size;
-	int rotate_num;
-	mslog_flush_mode_t flush_mode;
-    int enable_console_color;
-
-	size_t batch_threshold;
-	size_t max_batch_threshold;
-	size_t min_batch_threshold;
-	size_t log_bytes_per_sec;
-	time_t last_stat_time;
-	size_t curr_buf_used;
-	char *io_buf;
-
-	void *mmap_buf;
-	size_t mmap_size;
-	size_t mmap_offset;
-	size_t mmap_min_file_size;
-
-	mslog_mem_pool_t log_buf_pool;
-    mslog_thread_ctx_t flush_thread;
-
 	FILE *log_fp;
-	pthread_mutex_t level_mutex[MSLOG_LEVEL_MAX];
-	char time_cache[32];
-	time_t last_time;
+	mslog_level_t log_level;
+	mslog_flush_mode_t flush_mode;
+	size_t max_file_size;
+	int max_file_count;
+	size_t total_write_bytes;
+	int total_flush_time;
 	int rotate_check_cnt;
-	//char tag_filter[1024];
-};
+	pthread_mutex_t log_mutex;
+	int enable_console_color;
+    
+}mslog_global_t;
 
-typedef struct mslog_global mslog_global_t;
+extern mslog_global_t g_mslog;
 
 //default config marco
-#define MSLOG_DEFAULT_LOG_PATH			    ( "./mslog.log" )
-#define MSLOG_DEFAULT_LOG_LEVEL			    ( MSLOG_INFO )
-#define MSLOG_DEFAULT_ROTATE_SIZE		    ( 10 * 1024 * 1024 )
-#define MSLOG_DEFAULT_ROTATE_NUM		    ( 5 )
-#define MSLOG_DEFAULT_FLUSH_MODE		    ( MSLOG_FLUSH_BATCH )
-#define MSLOG_DEFAULT_ENABLE_CONSOLE_COLOR  ( 1 )
-
-#define MSLOG_DEFAULT_MIN_BATCH_THRESHOLD	( 1 * 1024 * 1024 )//1mb
-#define MSLOG_DEFAULT_MAX_BATCH_THRESHOLD	( 4 * 1024 * 1024 )//4mb
-#define MSLOG_DEFAULT_IO_BUF_SIZE           ( 4 * 1024 * 1024 )//4mb
-
-#define MSLOG_DEFAULT_MMAP_MIN_FILE_SIZE	( 1 * 1024 * 1024 )//1mb
-#define MSLOG_DEFAULT_MMAP_INIT_SIZE		( 4 * 1024 * 1024 )//4mb
-
-
-#define MSLOG_FLUSH_INTERVAL			    ( 1 )
-#define MSLOG_ROTATE_CHECK_CNT_MAX		    ( 100 )
+#define MSLOG_ROTATE_CHECK_MAX			( 100 )
+#define MSLOG_LOG_BUF_SIZE				( 2048 )
 
 //log output interface
-int mslog_init(const char *log_path, mslog_level_t log_level, size_t rotate_size, 
-        int rotate_num, mslog_flush_mode_t flush_mode, int enable_console_color);
-static inline int mslog_init_default(const char *log_path, mslog_level_t log_level, 
-        size_t rotate_size, int rotate_num, mslog_flush_mode_t flush_mode){
-    return mslog_init(log_path, log_level, rotate_size, rotate_num, flush_mode, MSLOG_DEFAULT_ENABLE_CONSOLE_COLOR);
-}
-void mslog_deinit();
+int mslog_init_default(const char *log_path, mslog_level_t log_level, 
+        size_t max_file_size, int max_file_count, mslog_flush_mode_t flush_mode);
+void mslog_deinit(void);
 void mslog_log(mslog_level_t level, const char *tag, const char *file, int line, 
         const char *func, const char *fmt, ...);
-void mslog_get_stats(size_t *total_write_bytes, size_t *avg_flush_time, size_t *buf_usage_rate);
 
 //log marco define
 #define MSLOG_DEBUG(tag, fmt, ...)\
@@ -119,40 +75,4 @@ void mslog_get_stats(size_t *total_write_bytes, size_t *avg_flush_time, size_t *
 #define MSLOG_FATAL(tag, fmt, ...)\
 	mslog_log(MSLOG_FATAL, tag, __FILE__, __LINE__, __func__, fmt, ##__VA_ARGS__)
 
-//static inline func
-static inline mslog_level_t mslog_get_level(void){
-	extern mslog_global_t g_mslog;
-	return g_mslog.log_level;
-}
-
-static inline void mslog_lock(mslog_level_t level){
-	extern mslog_global_t g_mslog;
-	if ( level >= 0 && level < MSLOG_LEVEL_MAX ){
-		pthread_mutex_lock(&g_mslog.level_mutex[level]);
-	}
-}
-
-static inline void mslog_unlock(mslog_level_t level){
-	extern mslog_global_t g_mslog;
-	if ( level >= 0 && level < MSLOG_LEVEL_MAX ){
-		pthread_mutex_unlock(&g_mslog.level_mutex[level]);
-	}
-}
-
-static inline const char *mslog_get_level_color(mslog_level_t level){
-    switch (level) {
-        case MSLOG_DEBUG:
-            return MSLOG_COLOR_DEBUG;
-        case MSLOG_INFO:
-            return MSLOG_COLOR_INFO;
-        case MSLOG_WARN:
-            return MSLOG_COLOR_WARN;
-        case MSLOG_ERROR:
-            return MSLOG_COLOR_ERROR;
-        case MSLOG_FATAL:
-            return MSLOG_COLOR_FATAL;
-        default:
-            return MSLOG_COLOR_RESET;
-    }
-}
 #endif//__MSLOG_H__
